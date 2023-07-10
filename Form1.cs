dd420a9ef5f6f02aed7d6e42f70ce08fe5853c9f
mrbc10 <miguelcleytonbru@gmail.com>
2023-07-10 18:24:57 -0300

    COMEÇO

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace projeto_André_Marachini
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
